51e25a718d85608b1c3e3cf70f692013f6d9db23
Anthony Colannino <anthony-colannino@MacBook-Pro.local>
2020-08-19 16:28:32 -0400

    mongodb connected

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+//Helps us connect to mongoDB database
 const mongoose = require('mongoose');
 //Configures environment variables
 require('dotenv').config();
@@ -12,6 +13,18 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 //Allows us to parse json
 app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+//Pass in our uri to connect to db
+mongoose.connect(uri, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true
+});
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB connected to successfully');
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
